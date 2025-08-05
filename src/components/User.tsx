56b8f6d0274a7265684bdf809ce8a04634057205
Lokonon52 <bilok456@gmail.com>
2025-08-05 04:57:31 +0100

    evenement click

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -13,7 +13,7 @@ function User({ id,name,email,sexe,avatar}: Props) {
   return (
     <div className="bg-zinc-50 border-zinc-200 border-2 rounded-lg text-zinc-800 items-center flex justify-evenly">
       {avatar ? (
-        <div className="img">
+        <div className="img my-1">
           <p className="text-2xl font-extrabold">profile{id}</p>
           <img src={avatar} alt={name} />
         </div>
@@ -23,7 +23,7 @@ function User({ id,name,email,sexe,avatar}: Props) {
         <ul>
           <li>{name}</li>
           <li>{email}</li>
-          <li className=" w-1/2  px-5 text-base text-fuchsia-600 bg-slate-50 border-zinc-200 border-2 rounded-lg">{sexe}</li>
+          <li className=" w-1/2  text-center text-base text-fuchsia-600 bg-slate-50 border-zinc-200 border-2 rounded-lg hover:bg-slate-100">{sexe}</li>
         </ul>
       ) : null}
     </div>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,10 +1,10 @@
 import User from "../components/User";
-type Sexe="homme"|"femme";
+type Sexe = "homme" | "femme";
 export type UserType = {
   id: number;
   name: string;
   email: string;
-  sexe?:Sexe;
+  sexe?: Sexe;
   avatar: string;
 };
 
@@ -12,25 +12,49 @@ type Props = {
   users: UserType[];
 };
 
-export const Users = (props: Props) => {
-  const { users } = props; //Destructurer props
+export const Users = ({ users }: Props) => {
+  let usersSorted:UserType[]=users; 
+  //fonction
+  const sortBy = (field: string) => {
+    console.log(field);
+    usersSorted.sort()
+  };
 
   return (
     <main>
-      <div className="my-6">
+      <div className="my-6 flex md:justify-end  gap-5 sm:justify-center">
         <span className="font-semibold">Trier par :</span>
-        <button className=" bg-emerald-300 mx-3 border-solid border-zinc-400 border-4 rounded-lg px-3 " type="button">HOMME</button>
-        <button  className=" bg-yellow-400 mx-3 border-solid border-zinc-400 border-4 rounded-lg px-3 " type="button">FEMME</button>
+        <button
+          className=" bg-emerald-300  border-solid border-zinc-400 border-2 rounded-lg px-3 hover:bg-emerald-100"
+          type="button"
+           onClick={()=> {sortBy('homme')}} //évènement
+         
+        >
+          HOMME
+        </button>
+        <button
+          className=" bg-yellow-400  border-solid border-zinc-400 border-2 rounded-lg px-3 hover:bg-yellow-100 "
+          type="button"
+           onClick={()=> {sortBy('femme')}} //évènement
+        >
+          FEMME
+        </button>
       </div>
-   <section className="grid gap-3 text-black sm:grid-cols-1 md:grid-cols-2 mb-10">
-      {users.map(({ id, name, email,sexe, avatar }: UserType) => (
-        <User key={id} avatar={avatar} name={name} email={email} sexe={sexe?.toUpperCase() as Sexe} id={id} />
-        //Ajoute toujours une key quand tu fais un .map() dans React :
-      ))}
 
-    </section>
+      <section className="grid gap-3 text-black sm:grid-cols-1 md:grid-cols-2 mb-10">
+        {usersSorted.map(({ id, name, email, sexe, avatar }: UserType) => (
+          <User
+            key={id}
+            avatar={avatar}
+            name={name}
+            email={email}
+            sexe={sexe?.toUpperCase() as Sexe}
+            id={id}
+          />
+          //Ajoute toujours une key quand tu fais un .map() dans React :
+        ))}
+      </section>
     </main>
- 
   );
 };
 
